e274e2dc2ea2162580ca0bf304d310add2f61b71
fujie <fujie@fe-cred.com>
2022-05-12 12:58:04 +0800

    mongo

---
+++ src/main/java/com/outletcn/app/model/mongo/GiftType.java
@@ -0,0 +1,30 @@
+package com.outletcn.app.model.mongo;
+
+/**
+ * @author felix
+ */
+public class GiftType {
+    private Long id;
+
+    /**
+     * 礼品类别
+     */
+    private Integer category;
+
+    /**
+     * 礼品类型
+     */
+    private String type;
+
+
+    /**
+     * 创建时间
+     */
+    private Long createTime;
+
+    /**
+     * 更新时间
+     */
+    private Long updateTime;
+
+}
