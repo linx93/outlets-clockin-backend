daf9c2f0379a3005c16bbd79e4c778ca42aac226
tanwei <tanwei@phadata.com>
2022-05-12 09:57:20 +0800

    :tada: 初始化项目.

---
+++ src/main/java/com/outletcn/app/OutletsClockinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.outletcn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutletsClockinBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutletsClockinBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/outletcn/app/OutletsClockinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outletcn.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutletsClockinBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
