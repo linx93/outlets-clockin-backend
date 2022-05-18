51fafbf2e65e83cf278c1c63ea621e4505d3ec6e
xionglin <xionglin@fe-cred.com>
2022-05-18 16:30:04 +0800

    1. 查询我的总签章

--- src/main/java/com/outletcn/app/controller/applet/ClockInUserController.java
+++ src/main/java/com/outletcn/app/controller/applet/ClockInUserController.java
@@ -7,14 +7,11 @@ import com.outletcn.app.model.dto.applet.AppletLoginRequest;
 import com.outletcn.app.model.dto.applet.UpdateUserRequest;
 import com.outletcn.app.service.AuthService;
 import com.outletcn.app.service.ClockInUserService;
+import com.outletcn.app.service.PunchLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -34,6 +31,7 @@ public class ClockInUserController {
 
     private final ClockInUserService clockInUserService;
     private final AuthService authService;
+    private final PunchLogService punchLogService;
 
 
     @ApiOperation(value = "更新打卡小程序用户的信息")
@@ -48,6 +46,13 @@ public class ClockInUserController {
     public ApiResult<LoginResponse> clockInLogin(@RequestBody @Valid AppletLoginRequest appletLoginRequest) {
         ApiResult<LoginResponse> apiResult = authService.clockInLogin(appletLoginRequest);
         return apiResult;
+    }
+
+    @ApiOperation(value = "我的总签章")
+    @GetMapping(value = "/my-score")
+    public ApiResult<Integer> myScore() {
+        Integer score = punchLogService.myScore();
+        return ApiResult.ok(score);
     }
 }
 
--- src/main/java/com/outletcn/app/service/impl/PunchLogServiceImpl.java
+++ src/main/java/com/outletcn/app/service/impl/PunchLogServiceImpl.java
@@ -1,15 +1,20 @@
 package com.outletcn.app.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.outletcn.app.model.dto.UserInfo;
 import com.outletcn.app.model.mysql.PunchLog;
 import com.outletcn.app.mapper.PunchLogMapper;
 import com.outletcn.app.service.PunchLogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.outletcn.app.utils.JwtUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
  * 打卡日志
- 服务实现类
+ * 服务实现类
  * </p>
  *
  * @author linx
@@ -18,4 +23,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class PunchLogServiceImpl extends ServiceImpl<PunchLogMapper, PunchLog> implements PunchLogService {
 
+    @Override
+    public Integer myScore() {
+        UserInfo info = JwtUtil.getInfo(UserInfo.class);
+        List<PunchLog> punchLogs = getBaseMapper().selectList(new QueryWrapper<PunchLog>().lambda().eq(PunchLog::getUserId, info.getId()));
+        return punchLogs.isEmpty() ? 0 : punchLogs.stream().mapToInt(PunchLog::getIntegralValue).sum();
+    }
 }
