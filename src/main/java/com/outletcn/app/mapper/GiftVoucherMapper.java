6a5f957fa829dfe5a39120bf519fd558bda14958
fujie <fujie@fe-cred.com>
2022-05-20 18:00:16 +0800

    :construction: 兑换逻辑补充

--- src/main/java/com/outletcn/app/mapper/GiftVoucherMapper.java
+++ src/main/java/com/outletcn/app/mapper/GiftVoucherMapper.java
@@ -2,6 +2,9 @@ package com.outletcn.app.mapper;
 
 import com.outletcn.app.model.mysql.GiftVoucher;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2022-05-12
  */
 public interface GiftVoucherMapper extends BaseMapper<GiftVoucher> {
+
+
+    List<GiftVoucher> findAllBy(@Param("giftId") Long giftId, @Param("userId") Long userId, @Param("sencond") Long sencond);
 
 }
--- src/main/java/com/outletcn/app/service/impl/PunchSignatureServiceImpl.java
+++ src/main/java/com/outletcn/app/service/impl/PunchSignatureServiceImpl.java
@@ -1,5 +1,6 @@
 package com.outletcn.app.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Sequence;
 import com.outletcn.app.common.PageInfo;
@@ -8,6 +9,7 @@ import com.outletcn.app.common.UserTypeEnum;
 import com.outletcn.app.converter.GiftConverter;
 import com.outletcn.app.exception.BasicException;
 import com.outletcn.app.mapper.GiftVoucherMapper;
+import com.outletcn.app.model.dto.UserInfo;
 import com.outletcn.app.model.dto.gift.GiftListResponse;
 import com.outletcn.app.model.dto.gift.GiftPunchSignatureResponse;
 import com.outletcn.app.model.mongo.Destination;
@@ -16,6 +18,7 @@ import com.outletcn.app.model.mongo.GiftBag;
 import com.outletcn.app.model.mysql.GiftVoucher;
 import com.outletcn.app.repository.PunchSignatureMongoRepository;
 import com.outletcn.app.service.PunchSignatureService;
+import com.outletcn.app.utils.JwtUtil;
 import com.outletcn.app.utils.QrcodeUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +29,8 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.UUID;
 
@@ -70,11 +75,47 @@ public class PunchSignatureServiceImpl implements PunchSignatureService {
 
     @Override
     public Boolean exchange(String giftId) {
+        //1查询礼品是否存在
+        GiftBag giftBag = mongoTemplate.findOne(Query.query(Criteria.where("id").is(Long.parseLong(giftId))), GiftBag.class);
 
-        GiftVoucher voucher = giftVoucherMapper.selectOne(new QueryWrapper<GiftVoucher>().lambda().eq(GiftVoucher::getGiftId, Long.parseLong(giftId)));
+        UserInfo userInfo = JwtUtil.getInfo(UserInfo.class);
+
+        if (giftBag == null) {
+            log.info("礼品不存在 {}", giftId);
+            throw new BasicException("礼品不存在");
+        }
+        //2查询礼品是否已经过期
+        LocalDateTime validDate = LocalDateTime.ofEpochSecond(giftBag.getValidDate(), 0, ZoneOffset.of("+8"));
+        if (validDate.isBefore(LocalDateTime.now())) {
+            log.info("礼品已过期 {}", giftId);
+            throw new BasicException("礼品已过期");
+        }
+
+
+        LambdaQueryWrapper<GiftVoucher> queryWrapper = new QueryWrapper<GiftVoucher>().lambda().eq(GiftVoucher::getGiftId, Long.parseLong(giftId)).eq(GiftVoucher::getUserId, Long.parseLong(userInfo.getId()));
+        List<GiftVoucher> voucher = giftVoucherMapper.selectList(queryWrapper);
+        //3查询礼品是否已经兑换
+        Integer exchangeCount = giftBag.getExchangeCount();
+
         if (voucher != null) {
-            log.info("礼品券已存在 giftId: {} voucher: {}", giftId, voucher.getId());
-            return Boolean.TRUE;
+            if (voucher.size() >= exchangeCount) {
+                log.info("礼品已兑换次数 {},礼品限制次数 {} ", voucher.size(), exchangeCount);
+                throw new BasicException("礼品已兑换次数");
+            }
+        }
+
+
+        long epochSecond = Instant.now().getEpochSecond();
+        List<GiftVoucher> exchangeLimitVoucher = giftVoucherMapper.findAllBy(Long.parseLong(giftId), Long.parseLong(userInfo.getId()), epochSecond);
+        //4查询礼品每日单次限制兑换次数
+        Integer exchangeLimit = giftBag.getExchangeLimit();
+
+        //TODO 带测试
+        if (exchangeLimitVoucher != null) {
+            log.info("礼品每日单次限制兑换次数 {},礼品限制次数 {} ", exchangeLimitVoucher.size(), exchangeLimit);
+            if (exchangeLimitVoucher.size() >= exchangeLimit) {
+                throw new BasicException("礼品每日单次兑换次数已用完");
+            }
         }
 
         String content = QRCodeContent.builder()
@@ -82,13 +123,9 @@ public class PunchSignatureServiceImpl implements PunchSignatureService {
                 .appId(UserTypeEnum.WRITE_OFF.name())
                 .type("1")
                 .build().toString();
-        GiftBag giftBag = mongoTemplate.findOne(Query.query(Criteria.where("id").is(Long.parseLong(giftId))), GiftBag.class);
+
         try {
             String qrcodeBase64 = QrcodeUtil.getQrcodeBase64(content);
-            if (giftBag == null) {
-                log.info("礼品不存在 {}", giftId);
-                throw new BasicException("礼品不存在");
-            }
 
             GiftVoucher giftVoucher = new GiftVoucher();
             giftVoucher.setGiftId(Long.parseLong(giftId));
@@ -96,8 +133,7 @@ public class PunchSignatureServiceImpl implements PunchSignatureService {
             giftVoucher.setGiftVoucherId(UUID.randomUUID().toString());
             giftVoucher.setGiftVoucherType(giftBag.getType());
             giftVoucher.setGiftVoucherQrcode(qrcodeBase64);
-            //TODO 获取用户信息ID
-            giftVoucher.setUserId(0L);
+            giftVoucher.setUserId(Long.parseLong(userInfo.getId()));
             //礼品券名称
             giftVoucher.setGiftVoucherName(giftBag.getName());
             giftVoucher.setExchangeDeadline(giftBag.getValidDate());
