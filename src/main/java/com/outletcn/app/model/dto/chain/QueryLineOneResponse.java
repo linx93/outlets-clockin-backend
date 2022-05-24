2868dec99b675620700d3cd502c01ccbea158774
fujie <fujie@fe-cred.com>
2022-05-24 14:51:04 +0800

    :construction: 添加swagger 描述

--- src/main/java/com/outletcn/app/model/dto/chain/QueryLineOneResponse.java
+++ src/main/java/com/outletcn/app/model/dto/chain/QueryLineOneResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.outletcn.app.model.mongo.DetailObjectType;
 import com.outletcn.app.model.mongo.Line;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -26,9 +27,13 @@ public class QueryLineOneResponse {
     public static class Item {
         @JsonSerialize(using = ToStringSerializer.class)
         private Long id;
+        @ApiModelProperty(value = "目的地/目的地群")
         private String name;
+        @ApiModelProperty(value = "目的地/目的地群")
         private String type;
+        @ApiModelProperty(value = "目的地/目的地群属性")
         private List<String> attr;
+        @ApiModelProperty(value = "分值")
         private Integer score;
     }
 }
--- src/main/java/com/outletcn/app/model/mongo/DetailObjectType.java
+++ src/main/java/com/outletcn/app/model/mongo/DetailObjectType.java
@@ -2,6 +2,7 @@ package com.outletcn.app.model.mongo;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -19,45 +20,52 @@ public class DetailObjectType implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty(value = "ID")
     @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     /**
      * 主体ID
      */
+    @ApiModelProperty(value = "主体ID")
     @JsonSerialize(using = ToStringSerializer.class)
     private Long objectId;
 
     /**
      * 主体类型
      */
+    @ApiModelProperty(value = "主体类型")
     private int objectType;
 
     /**
      * 推荐视频
      */
+    @ApiModelProperty(value = "推荐视频")
     private String recommendVideo;
 
     /**
      * 推荐音频
      */
-
+    @ApiModelProperty(value = "推荐音频")
     private String recommendAudio;
 
     /**
      * 描述
      */
+    @ApiModelProperty(value = "描述")
     private List<Map<String, Object>> descriptions;
 
 
     /**
      * 创建时间
      */
+    @ApiModelProperty(value = "创建时间")
     private Long createTime;
 
     /**
      * 更新时间
      */
+    @ApiModelProperty(value = "更新时间")
     private Long updateTime;
 
 }
